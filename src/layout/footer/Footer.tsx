fe1a4c4de3b18f0c124c6867c39c072ae41d3fd4
klevchuk <lemoniklava@gmail.com>
2023-09-23 20:10:37 +0800

    footer styles

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from "styled-components";
 import {Icon} from "../../componend/icon/Icon";
 import {FlexWrapper} from "../../componend/FlexWrapper";
+import {theme} from "../../styles/Theme";
 
 export const Footer = () => {
     return (
@@ -37,24 +38,45 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-  background-color: #ffd8a5;
-  min-height: 20vh;
+  background-color: ${theme.colors.primaryBg};
+  padding: 40px 0;
 `
 const Name = styled.span`
-
+  font-family: Josefin Sans, sans-serif;
+  font-size: 22px;
+  font-weight: 700;
+  letter-spacing: 3px;
 `
 
 const SocialList = styled.ul`
   display: flex;
-  gap: 30px;
+  gap: 20px;
+  margin: 30px 0;
 `
 
 const SocialLink = styled.a`
-
+  background-color: rgba(225, 225, 225, 0.1);
+  border-radius: 50%;
+  width: 35px;
+  height: 35px;
+  
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  
+  color: ${theme.colors.accent};
+  
+  &:hover {
+    color: ${theme.colors.primaryBg};
+    transform: translateY(-4px);
+  }
 `
 
 const Copyright = styled.small`
-
+  text-align: center;
+  font-size: 12px;
+  font-weight: 400;
+  opacity: 0.5;
 `
 
 const SocialItem = styled.li`
