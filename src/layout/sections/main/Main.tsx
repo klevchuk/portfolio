3f48b80b60785a67aa7a62186d540ede0fb3ca88
klevchuk <lemoniklava@gmail.com>
2023-09-30 23:38:09 +0800

    main adapt

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -10,7 +10,7 @@ export const Main = () => {
     return (
         <StyledMain>
             <Container>
-                <FlexWrapper align={"center"} justify={"space-between"} wrap={"wrap"}>
+                <FlexWrapper align={"center"} justify={"space-around"} wrap={"wrap"}>
                     <div>
                         <SmallText>Hi There</SmallText>
                         <Name>I am <span>Vlad Klevchuk</span></Name>
@@ -36,6 +36,7 @@ const StyledMain = styled.section`
 const PhotoWrapper = styled.div`
 position: relative;
   z-index: 0;
+  margin-top: 65px;
   
   &::before {
     content: "";
@@ -51,6 +52,8 @@ position: relative;
     @media ${theme.media.mobile} {
       width: 314px;
       height: 414px;
+      top: -17px;
+      left: 20px;
     }
   }
 `
@@ -60,6 +63,7 @@ const Photo = styled.img`
   width: 350px;
   height: 430px;
   object-fit: cover;
+  margin-right: 20px;
   
   @media ${theme.media.mobile} {
     width: 310px;
@@ -68,21 +72,19 @@ const Photo = styled.img`
 `
 
 const MainTitle = styled.h1`
-  font-size: 27px;
-  font-weight: 400;
+  ${font({ weight: 400, Fmax: 27, Fmin: 20})}
 `
 
 const Name = styled.h2`
   ${font({family: "Josefin Sans, sans-serif", weight: 700, Fmax: 50, Fmin: 36})}
-  //font-family: Josefin Sans, sans-serif;
-  //font-size: 50px;
-  //font-weight: 700;
   letter-spacing: 0.05em;
   margin: 10px 0;
   
   span {
     position: relative;
     z-index: 0;
+    white-space: nowrap;
+    
     &::before {
       content: "";
       display: inline-block;
@@ -94,6 +96,11 @@ const Name = styled.h2`
       bottom: 0;
       z-index: -1;
     }
+  }
+  
+  @media ${theme.media.mobile} {
+    margin: 15px 0 22px;
+    
   }
 `
 
